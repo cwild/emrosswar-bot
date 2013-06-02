885dbce6bc86408aa58d574824b4fc83ecb07b5b
Chris Wild <chris@cryformercy.com>
2013-06-02 03:23:17 +0100

    Underlying _data dict does not contain a `ret` key.

--- emross/utility/countdown.py
+++ emross/utility/countdown.py
@@ -55,7 +55,7 @@ class CountdownManager(EmrossBaseObject, CacheableData):
 
     def add_tasks(self, tasks):
         tasks = self._normalise(tasks)
-        self._data['ret']['cdlist'].extend(tasks)
+        self._data['cdlist'].extend(tasks)
 
         """
         The Emross client does this when one of the events has completed.
