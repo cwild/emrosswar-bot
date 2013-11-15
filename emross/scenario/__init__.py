34a9a42041f98f2bab0e44db958a3cfb127b1532
Chris Wild <chris@cryformercy.com>
2013-11-15 23:49:27 +0000

    Fetching scenario client data uses '{0}'.format() instead of '%d' % ('1',) because it was failing on type conversions.

--- emross/scenario/__init__.py
+++ emross/scenario/__init__.py
@@ -3,12 +3,12 @@ from emross.api.cache import EmrossDataHandler
 
 class ScenarioDataHandler(EmrossDataHandler):
     def map_name(self, scenario=0):
-        return self.data['campaign']['INFO']['campaign_%d_mapname' % scenario]
+        return self.data['campaign']['INFO']['campaign_{0}_mapname'.format(scenario)]
 
     def point_name(self, scenario=0, point=1):
         try:
-            return self.data['campaign']['INFO']['campaign_%d_npclist_%d_pointname' % (scenario, point)]
+            return self.data['campaign']['INFO']['campaign_{0}_npclist_{1}_pointname'.format(scenario, point)]
         except KeyError:
-            return 'UNKNOWN point (%d)' % point
+            return 'UNKNOWN point ({0})'.format(point)
 
 EmrossWar.extend('SCENARIO_TEXT', 'translation/%(lang)s/campaign.lng.js', model=ScenarioDataHandler)
