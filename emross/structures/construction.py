b43634f787699406c3d4d65ed16f05427e9e7de5
Chris Wild <chris@cryformercy.com>
2014-01-08 23:24:30 +0000

    Added an `open_slots` option to Construct. If specified, allow the bot to use a "Bless of Build I/II" to allow further building to begin.

--- emross/structures/construction.py
+++ emross/structures/construction.py
@@ -1,15 +1,12 @@
 from emross.api import EmrossWar
 from emross.item import inventory
+from emross.structures.buildings import Building
 from emross.utility.task import FilterableCityTask, TaskType
-
-from buildings import Building
-
-import logging
-logger = logging.getLogger(__name__)
 
 class Construct(FilterableCityTask):
     CREATE_TASK_URL = 'game/building_create_task_api.php'
     EXTRA_SLOT_ITEMS = (inventory.BLESS_OF_BUILDING_I[0], inventory.BLESS_OF_BUILDING_II[0])
+    MAX_BUILD_SLOTS = 3
 
     COUNTDOWN_ITEMS = [
         (inventory.FAST_BUILDING_I[0], 900),
@@ -21,27 +18,35 @@ class Construct(FilterableCityTask):
         """
         {"code":0,"ret":{"cdlist":[{"id":1234567,"cdtype":1,"target":"5","owner":0,"secs":90}]}}
         """
-        json = self.bot.api.call(self.__class__.CREATE_TASK_URL, city=city.id, build_type=build_type)
-        name = EmrossWar.BUILDING[str(build_type)].get('name', 'BUILD_TYPE %d' % build_type)
+        json = self.bot.api.call(self.CREATE_TASK_URL, city=city.id, build_type=build_type)
+        name = EmrossWar.BUILDING[str(build_type)].get('name', 'BUILD_TYPE {0}'.format(build_type))
         if json['code'] == EmrossWar.SUCCESS:
-            logger.info('Started building "%s" at "%s". Time until completion: %d seconds.' % (name, city.name, json['ret']['cdlist'][0]['secs']))
+            self.log.info('Started building "{0}" at "{1}". Time until completion: {2} seconds.'.format(name, city.name, json['ret']['cdlist'][0]['secs']))
         else:
-            logger.debug('Failed to upgrade build_type "%s" at city "%s"' % (name, city.name))
+            self.log.debug('Failed to upgrade "{0}" at city "{1}"'.format(name, city.name))
         return json
 
     def downgrade(self, city, build_type):
-        return self.bot.api.call(self.__class__.CREATE_TASK_URL, city=city.id, build_type=build_type, build_act='destroy')
+        return self.bot.api.call(self.CREATE_TASK_URL, city=city.id, build_type=build_type, build_act='destroy')
 
     def structure_level(self, city, building):
         return city.data[Building.OFFSET[building]]
 
-    def process(self, structure, level, use_scrolls=False, *args, **kwargs):
+    def process(self, structure, level,
+        use_scrolls=False,
+        open_slots=False,
+        *args, **kwargs):
+        """
+        Go through each city and see if we can build the desired building to the target level.
+        """
+
+        # We should continue
         result = True
 
         for city in self.cities(**kwargs):
 
             if city.data[0] == 0:
-                logger.info('City "%s" is out of free land. Unable to build.' % city.name)
+                self.log.info('City "{0}" is out of free land. Unable to build.'.format(city.name))
                 continue
 
             current_level = self.structure_level(city, structure)
@@ -50,13 +55,40 @@ class Construct(FilterableCityTask):
 
                 buffs = city.data[23]
                 capacity = 1 + len([b for b in buffs if b['itemid'] in self.EXTRA_SLOT_ITEMS])
-                logger.debug('Build capacity at castle "%s" is %d' % (city.name, capacity))
+                self.log.debug('Build capacity at castle "{0}" is {1}'.format(city.name, capacity))
 
                 current_build = dict([(t['target'], t) for t in tasks])
 
-                if structure in current_build and use_scrolls:
-                    task = current_build[structure]
-                    city.countdown_manager.use_items_for_task(task, self.COUNTDOWN_ITEMS)
+                if structure in current_build:
+
+                    if use_scrolls:
+                        task = current_build[structure]
+                        city.countdown_manager.use_items_for_task(task, self.COUNTDOWN_ITEMS)
+
+                elif open_slots and self.MAX_BUILD_SLOTS > len(tasks) == capacity:
+                    """
+                    Not already building this structure at this city.
+                    Slots are full but we could open more
+                    """
+                    build_buffs = self.bot.find_inventory_items(self.EXTRA_SLOT_ITEMS)
+
+                    done = False
+                    for sid, vals in build_buffs.iteritems():
+                        for item_id, qty, price in vals:
+
+                            self.log.info('Open another build slot by using a "{0}"'.format(EmrossWar.ITEM[str(sid)].get('name')))
+                            json = self.bot.item_manager.use(city=city.id, id=item_id, num=1)
+
+                            if json['code'] == EmrossWar.SUCCESS:
+                                capacity += 1
+                                # we should have a new buff which applies to all cities
+                                for _city in self.bot.cities:
+                                    _city.expire()
+                                done = True
+                                break
+
+                        if done:
+                            break
 
                 if len(tasks) < capacity and structure not in current_build \
                     and city.resource_manager.meet_requirements(Building.cost(structure, current_level+1), **kwargs):
