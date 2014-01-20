dc27f037ba714fc57bc243405f284e8219ded80e
Chris Wild <chris@cryformercy.com>
2014-01-20 10:55:35 +0000

    Added error testing for Hero loyalty raiser. If we hit our failure threshold then we assume that further heroes would fail as well so don't try to raise their loyalty.

--- emross/arena/loyalty.py
+++ emross/arena/loyalty.py
@@ -13,17 +13,22 @@ class AutoLoyalty(FilterableCityTask):
     LOYALTY_COST = 1000
     LOYALTY_PER_DAY = 5
     MAX_LOYALTY = 100
+    FAILURE_THRESHOLD = 3
+    FAILURE_THRESHOLD_PER_CITY = 1
 
     DAILY_HERO_REWARD_LIMIT = 1611
 
 
-    def process(self, below=100, revive=True, sleep=(10,12), *args, **kwargs):
+    def process(self, below=MAX_LOYALTY, revive=True, sleep=(1,3),  *args, **kwargs):
         """
         Try to raise our heroes loyalty to the max!
         """
 
-        cities = self.cities(**kwargs)
-        for city in cities:
+        total_failures = 0
+        for city in self.cities(**kwargs):
+            # Track failures per city
+            failures = 0
+
             for hero in city.hero_manager.heroes.itervalues():
                 if hero.data.get(Hero.STATE) == Hero.DEAD:
                     if not revive:
@@ -33,6 +38,10 @@ class AutoLoyalty(FilterableCityTask):
                     cost = REVIVAL_COST * hero.data.get(Hero.LEVEL)
                     if city.resource_manager.meet_requirements({Resource.GOLD: cost}, **kwargs):
                         city.hero_manager.revive_hero(hero)
+
+                if total_failures == self.FAILURE_THRESHOLD or failures == self.FAILURE_THRESHOLD_PER_CITY:
+                    # Assume that we are not yet in a position to raise loyalty on any heroes
+                    continue
 
                 loyalty = int(hero.data.get(Hero.LOYALTY, 0))
                 self.log.info('{0} has {1} {2}.'.format(hero, loyalty, LOYALTY))
@@ -53,5 +62,7 @@ class AutoLoyalty(FilterableCityTask):
                                     gold = city.resource_manager.get_amount_of(Resource.GOLD)
                                     city.resource_manager.set_amount_of(Resource.GOLD, gold-self.LOYALTY_COST)
                                 elif json['code'] == self.DAILY_HERO_REWARD_LIMIT:
+                                    total_failures += 1
+                                    failures += 1
                                     self.log.info('Unable to raise {0} any further for {1} at the moment'.format(LOYALTY, hero))
                                     break
