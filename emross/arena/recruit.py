7337ab637a30e454cd813c41b27c93f2aec760c7
Chris Wild <chris@cryformercy.com>
2014-02-07 00:09:15 +0000

    Improve HeroRecruit so hero ranks can be specified.

--- emross/arena/recruit.py
+++ emross/arena/recruit.py
@@ -1,3 +1,7 @@
+from __future__ import division
+
+import math
+
 from emross.api import EmrossWar
 from emross.arena import CONSCRIPT_URL
 from emross.arena.hero import Hero
@@ -12,31 +16,35 @@ RECRUIT = EmrossWar.TRANSLATE['f_city_hero'].get('3', 'Recruit')
 class HeroRecruit(FilterableCityTask):
     INTERVAL = 3600
 
-    def process(self, recruit_heroes=[], *args, **kwargs):
+    def process(self, recruit_heroes=[], recruit_hero_ranks=[], *args, **kwargs):
 
-        cities = self.cities(**kwargs)
         st = self.bot.builder.task(Construct)
         delays = []
-        for city in cities:
-            if st.structure_level(city, Building.ARENA) < 1:
-                self.log.info('There is no arena at {0}'.format(city.name))
+
+        for city in self.cities(**kwargs):
+            arena = st.structure_level(city, Building.ARENA)
+
+            if arena < 1:
+                self.log.debug('There is no arena at "{0}"'.format(city.name))
+                continue
+
+            capacity = math.ceil(arena / 2)
+            if capacity <= len(city.hero_manager.heroes):
+                self.log.debug('There is no space to recruit any further heroes at "{0}"'.format(city.name))
                 continue
 
             self.log.info('Check for heroes at the bar in "{0}"'.format(city.name))
             json = self.bot.api.call(CONSCRIPT_URL, city=city.id)
 
-            gold = None
-            if 'price' in json['ret']:
-                gold = int(json['ret']['price'])
 
-            if 'refresh' in json['ret']:
-                self.log.info('We have to wait before we can do this. Timer: {0}'.format(json['ret']['refresh']))
-                delays.append(int(json['ret']['refresh']))
-            elif gold:
-                if not city.resource_manager.meet_requirements({Resource.GOLD: gold}, **kwargs):
-                    delays.append(300)
-                    continue
+            gold = int(json['ret'].get('price', 0))
 
+            if gold and not city.resource_manager.meet_requirements({Resource.GOLD: gold}, **kwargs):
+                delays.append(300)
+                continue
+
+
+            if 'refresh' not in json['ret']:
                 self.log.info('Try buying a drink')
                 json = self.bot.api.call(CONSCRIPT_URL, city=city.id, action='pub_process')
 
@@ -47,19 +55,27 @@ class HeroRecruit(FilterableCityTask):
                 if json['code'] == EmrossWar.INSUFFICIENT_GOLD:
                     self.log.info('Insufficient gold to buy a drink!')
                     delays.append(300)
-                else:
-                    delays.append(int(json['ret']['refresh']))
 
 
-                if 'hero' in json['ret'] and json['ret']['hero']['gid'] in recruit_heroes:
-                    hero = Hero(json['ret']['hero']['gid'])
-                    self.log.info('Found a hero we are looking for: {0}'.format(hero.client.get('name')))
+            delays.append(int(json['ret']['refresh']))
+
+            if 'hero' in json['ret']:
+                hero = Hero(json['ret']['hero'])
+
+                recruit_conditions = [
+                    hero.data.get('gid') in recruit_heroes,
+                    hero.client.get('rank') in recruit_hero_ranks,
+                ]
+
+                if any(recruit_conditions):
+                    self.log.info('Found a desired hero to recruit: {0}'.format(hero))
+
                     json = self.bot.api.call(CONSCRIPT_URL, city=city.id, action='hire_process')
 
                     if json['code'] == EmrossWar.SUCCESS:
-                        self.log.info('{0} recruited!'.format(hero))
+                        self.log.info('"{0}" recruited at "{1}"!'.format(hero, city.name))
                     else:
-                        self.log.info('Could not recruit {0}'.format(hero))
+                        self.log.info('Could not recruit "{0}"'.format(hero))
 
         if delays:
             self.sleep(min(delays))
