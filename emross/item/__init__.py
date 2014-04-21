dcbf9fc244218a37a9be302a6b9f0fd19168b141
Chris Wild <chris@cryformercy.com>
2014-04-21 22:09:04 +0100

    Added some EmrossWar.ITEM methods for future use. This has been awaiting commit for a while and I wanted it out of the way.

--- emross/item/__init__.py
+++ emross/item/__init__.py
@@ -1,2 +1,20 @@
+import re
+
 from emross.api import EmrossWar
-EmrossWar.extend('ITEM', 'translation/%(lang)s/item_data.js')
+from emross.api.cache import EmrossDataHandler
+
+
+class ItemDataHandler(EmrossDataHandler):
+    iteritems = lambda self: self.data.iteritems()
+
+    def name(self, name=None):
+        for item, data in self.iteritems():
+            if data['name'] == name:
+                return item, data
+
+    def search(self, name=None):
+        for item, data in self.iteritems():
+            if re.match(name, data['name'], re.IGNORECASE):
+                return item, data
+
+EmrossWar.extend('ITEM', 'translation/%(lang)s/item_data.js', model=ItemDataHandler)
