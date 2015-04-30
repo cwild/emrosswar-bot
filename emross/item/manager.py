f2b206f598d9e6bf121f75906e058e7cb0586d8d
Chris Wild <chris@cryformercy.com>
2015-05-01 00:46:09 +0100

    Try to find item names from client data if the item usage API doesn't give us the name of it.

--- emross/item/manager.py
+++ emross/item/manager.py
@@ -181,7 +181,11 @@ class InventoryManager(Controllable, CacheableData):
                        feedback.append((json['ret'].get('gem'), EmrossWar.LANG['MONEY']))
 
                        for _item in json['ret'].get('item', []):
-                           feedback.append((_item['num'], _item['name']))
+                           feedback.append((_item['num'],
+                               _item.get('name') or
+                               EmrossWar.ITEM.data.get(str(_item['sid']), {}).get('name') or
+                               'sid:{0}'.format(_item['sid'])
+                           ))
 
                        msg = ['{0}*{1}'.format(qty, name) for qty, name in feedback if qty]
                        if msg:
