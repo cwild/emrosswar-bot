cd77999fb5fc2d4da0eeaa8048af85cd49177e84
Chris Wild <chris@cryformercy.com>
2015-12-05 22:40:00 +0000

    Add a delay between HTTP 503 errors.

--- emross/handlers/http_errors.py
+++ emross/handlers/http_errors.py
@@ -1,19 +1,24 @@
 import logging
-logger = logging.getLogger(__name__)
+import time
 
 from emross.exceptions import EmrossWarException
 from emross.handlers.handler import EmrossHandler
 from emross.handlers.client_errors import InvalidKeyHandler
 
+logger = logging.getLogger(__name__)
+
 
 class ServiceUnavailableHandler(EmrossHandler):
     HTTP_STATUS_CODE = 503
+    RETRIES = 3
+    DELAY = 30
 
     def process(self, errors):
         count = len([status for status, data in errors if status == self.HTTP_STATUS_CODE])
 
-        if count < 3:
+        if count < self.RETRIES:
             logger.debug('Error may be temporary (current count %d)' % count)
+            time.sleep(self.DELAY)
         else:
             logger.debug('We keep seeing HTTP error %d; try relogging to clear it' % self.HTTP_STATUS_CODE)
 
